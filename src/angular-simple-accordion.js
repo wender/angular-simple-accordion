739f0e74ca8a7e5a633ff3701733efd04f6c61fd
Wender Lima <wender.lima@gmail.com>
2015-11-10 14:29:01 -0200

    Sometimes using parseAsHtml the directive loads before the content

--- src/angular-simple-accordion.js
+++ src/angular-simple-accordion.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('simple-accordion', [])
-    .directive('simpleAccordion', function($compile) {
+    .directive('simpleAccordion', function($compile, $timeout) {
         return {
             restrict: 'AEC',
-            controller: function($scope, $element) {
+            controller: function($scope) {
                 $scope.current = null;
                 $scope.height = [];
                 $scope.zero = {
@@ -15,29 +15,30 @@ angular.module('simple-accordion', [])
                 };
             },
             link: function(scope, el, attrs) {
-                var itemSelector = attrs.itemSelector || 'li',
-                    titleSelector = attrs.titleSelector || 'h2',
-                    contentSelector = attrs.contentSelector || 'p';
-
+              var itemSelector = attrs.itemSelector || 'li',
+                  titleSelector = attrs.titleSelector || 'h2',
+                  contentSelector = attrs.contentSelector || 'p';
+              $timeout(function(){
                 var items = el[0].querySelectorAll(itemSelector);
                 for (var i in items) {
-                    if (angular.isObject(items[i])) {
-                        var title = items[i].querySelectorAll(titleSelector)[0];
-                        var content = items[i].querySelectorAll(contentSelector)[0];
-                        scope.height.push({
-                            'height': (content.offsetHeight + 10) + 'px'
-                        });
-                        angular.element(items[i]).addClass('item').attr({
-                            'ng-class': '{\'open\':current == ' + i + '}'
-                        });
-                        angular.element(title).addClass('title').attr('ng-click', 'toggle(' + i + ')');
-                        angular.element(content).addClass('content').attr({
-                            'ng-style': 'current == ' + i + '?height[' + i + ']:zero'
-                        });;
+                  if (angular.isObject(items[i])) {
+                    var title = items[i].querySelectorAll(titleSelector)[0];
+                    var content = items[i].querySelectorAll(contentSelector)[0];
+                    scope.height.push({
+                      'height': (content.offsetHeight + 10) + 'px'
+                    });
+                    angular.element(items[i]).addClass('item').attr({
+                      'ng-class': '{\'open\':current == ' + i + '}'
+                    });
+                    angular.element(title).addClass('title').attr('ng-click', 'toggle(' + i + ')');
+                    angular.element(content).addClass('content').attr({
+                      'ng-style': 'current == ' + i + '?height[' + i + ']:zero'
+                    });;
 
-                    }
+                  }
                 }
                 $compile(angular.element(el).contents())(scope);
+              });
             }
         }
     });
